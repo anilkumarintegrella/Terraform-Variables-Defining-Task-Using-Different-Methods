e71489ca2a81df75da5f01330b83841dd11af444
Anilkumar Vaghari <anilkumarintegrella>
2023-05-17 19:07:46 +0530

    added Method1

---
+++ Method1/README.md
@@ -0,0 +1,3 @@
+## Method 1
+
+- Variables declared via seperate `variables.tf` file and defined it by default in same file
---
+++ Method1/variables.tf
@@ -0,0 +1,15 @@
+
+variable "rg_name" {
+  type    = string
+  default = "rg-anil-vaghari-playground"
+}
+
+variable "location" {
+  type    = string
+  default = "West Europe"
+}
+
+variable "dns_zone" {
+  type    = string
+  default = "anilv.azure.integrella.net"
+}
